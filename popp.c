abad667611d4c37f5f3663b6e32125617e8c3c77
Atikunaja <najaatiku1@gmail.com>
2023-10-18 17:57:15 -0700

    update

--- popp.c
+++ popp.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_popp - prints the top
+ * f_pop - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
--- print.c
+++ print.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_print - prints the top
+ * f_pint - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
